19d7919296cf7e9b9d19b7851ed8a19b038ae755
mitia-sv <dimitrisveloso@gmail.com>
2021-09-21 19:23:56 -0300

    Getters and Setters classTable

--- src/entities/Table.java
+++ src/entities/Table.java
@@ -4,6 +4,7 @@ import entities.enums.TableStatus;
 
 public class Table {
 
+	//Pode ser necessario uma list
     private Card[] cardsOnTable = new Card[5];
 
     private TableStatus status;
@@ -13,5 +14,22 @@ public class Table {
         this.cardsOnTable = cardsOnTable;
     }
 
-    // TODO getters and setters
+	public Card[] getCardsOnTable() {
+		return cardsOnTable;
+	}
+
+	//Pode ser necessario uma list
+	public void setCardsOnTable(Card[] cardsOnTable) {
+		this.cardsOnTable = cardsOnTable;
+	}
+
+	public TableStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(TableStatus status) {
+		this.status = status;
+	}
+
+    
 }
