0f4d8bbf92b76a1b7bd0f1dcb7e3cce2952ba09e
Ricardo271 <ric.zanluchi@gmail.com>
2021-10-19 10:38:37 -0300

    Definição das classes Deck e Table

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,37 @@
 package aplication;
 
+import entities.Card;
+import entities.Deck;
 import entities.Hand;
 import entities.Player;
+import entities.Table;
 
 public class Program {
     
     public static void main(String[] args) {
-    	
-    }
 
+        Deck deck = new Deck();
+        deck.shuffle();
+
+        Table table = new Table();
+
+        for (Card card : deck.getDeck()) {
+            System.out.print(card.toString() + " ");
+        }
+        System.out.println();
+
+        System.out.println(table.toString());
+        table.revealCard(deck.removeCard());
+
+        for (Card card : deck.getDeck()) {
+            System.out.print(card.toString() + " ");
+        }
+        System.out.println();
+
+        System.out.println(table.toString());
+
+    }
+/*
     public static void round() {
 
         int playerNumber = 3;
@@ -20,5 +43,5 @@ public class Program {
         }
 
     }
-
+*/
 }
--- src/entities/Table.java
+++ src/entities/Table.java
@@ -2,7 +2,11 @@ package entities;
 
 public class Table {
 
-	private Card[] cardsOnTable = new Card[2];
+	private Card[] cardsOnTable = new Card[5];
+
+	public Table() {
+
+	}
 
 	public Card[] getCardsOnTable() {
 		return this.cardsOnTable;
@@ -12,4 +16,31 @@ public class Table {
 		this.cardsOnTable = cardsOnTable;
 	}
 
+	public void setRandomTable() {
+
+		// TODO
+
+	}
+
+	public void revealCard(Card card) {
+
+		for (int i = 0; i < cardsOnTable.length; i++) {
+			if (cardsOnTable[i] == null) {
+				cardsOnTable[i] = card;
+				return;
+			}
+		}
+		// TODO excecao para quando todas as cartas já tiverem sido reveladas
+
+	}
+
+	public String toString() {
+		String output = "";
+		for (Card card : cardsOnTable) {
+			if (card != null) {
+				output += card.toString() + " ";
+			}
+		}
+		return output;
+	}
 }
