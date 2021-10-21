9d1650411832d873b9efe770b8dae2e3e88dc9db
mitia-sv <dimitrisveloso@gmail.com>
2021-10-21 19:50:09 -0300

    atualização Program.java

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,6 +1,8 @@
 package aplication;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import entities.Card;
 import entities.Deck;
@@ -9,10 +11,14 @@ import entities.Player;
 import entities.Table;
 
 public class Program {
+	
+	static Deck deck = new Deck();
     
     public static void main(String[] args) {   	
       
-      inicialMenu();
+    	
+    	deck.shuffle();
+    	inicialMenu();
     
     }
     
@@ -51,11 +57,33 @@ public class Program {
         int playerNumber = sc.nextInt();
         Player[] players = new Player[playerNumber];
 
+        List<Card> tableCards = new ArrayList<Card>();
+        for (int i = 0; i < 5; i++) 
+        	tableCards.add(deck.removeCard());
+        
         for (int i = 0; i < playerNumber; i++) {
-            String[] cardsOnHand = {};
-            players[i] = new Player(i, new Hand(cardsOnHand));
+            List<Card> playerHand = new ArrayList<Card>();
+            for (int j = 0; j < 2; j++)
+            	playerHand.add(deck.removeCard());
+            
+            players[i] = new Player(i, new Hand(merge(playerHand, tableCards)));
+            players[i] = new Player(i, tableCards, playerHand);
+            
+            System.out.printf("\nMão do Jogador %d: %s\n", i+1, playerHand.toString());
+            System.out.println("Quanto ele pagou? (digite 0 se ele saiu da mesa) $");
+            
         }
-
+        
+    }
+    
+    public static<T> List<T> merge(List<T> list1, List<T> list2)
+    {
+        List<T> list = new ArrayList<>();
+     
+        list.addAll(list1);
+        list.addAll(list2);
+     
+        return list;
     }
 
 }
