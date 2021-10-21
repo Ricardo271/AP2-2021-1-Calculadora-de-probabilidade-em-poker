219d8dd51d6403a35fe63e995d7ce4c32375b560
mitia-sv <dimitrisveloso@gmail.com>
2021-10-21 18:38:14 -0300

    atualização do program.java

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -19,39 +19,43 @@ public class Program {
     public static void inicialMenu() {
 		
     	System.out.printf("------- Poker Scores ------- \n\n");
-    	System.out.println("Select a function:");
-    	System.out.printf("N - New Game \nL - Leave Program\n");
+    	System.out.println("Selecione uma Função");
+    	System.out.printf("N - Novo Jogo \nS - Sair do Programa\n");
     	
     	try (Scanner entry = new Scanner(System.in)) {
 			  String resposta = entry.next();
 			
-			  if("L".equals(resposta)) {
+			  if("S".equals(resposta)) {
 
-				  System.out.println("Program ended");
+				  System.out.println("Programa Encerrado");
 				  System.exit(0);
 			  }
 			  if("N".equals(resposta)) {
 				  round();	
 			  }
 			  else {
-				  System.out.printf("\nPlease, digit a valid Letter (In Capslock)");
+				  System.out.printf("\nDigite uma letra válida (em maiúsculo)");
 				
 			  }
-		  }
+		}
 	}
 
 
-/*
-    public static void round() {
 
-        int playerNumber = 3;
+    public static void round() {
+    	
+    	System.out.printf("------- Poker Scores ------- \n\n");
+    	System.out.printf("Digite o número de jogadores: ");
+    	Scanner sc = new Scanner(System.in);
+    	
+        int playerNumber = sc.nextInt();
         Player[] players = new Player[playerNumber];
 
         for (int i = 0; i < playerNumber; i++) {
-            String[] cardsOnHand = {"9s", "Ac"};
+            String[] cardsOnHand = {};
             players[i] = new Player(i, new Hand(cardsOnHand));
         }
 
     }
-*/
+
 }
