aec095c9469908f6237edbecbcb106e28735e0b7
mitia-sv <dimitrisveloso@gmail.com>
2021-10-27 16:35:20 -0300

    Rodadas de apostas no program.java

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -29,7 +29,7 @@ public class Program {
     	System.out.printf("N - Novo Jogo \nS - Sair do Programa\n");
     	
     	try (Scanner entry = new Scanner(System.in)) {
-			  String resposta = entry.next();
+			  String resposta = entry.next().toUpperCase();
 			
 			  if("S".equals(resposta)) {
 
@@ -39,10 +39,7 @@ public class Program {
 			  if("N".equals(resposta)) {
 				  round();	
 			  }
-			  else {
-				  System.out.printf("\nDigite uma letra válida (em maiúsculo)");
-				
-			  }
+			
 		}
 	}
 
@@ -50,7 +47,7 @@ public class Program {
 
     public static void round() {
     	
-    	System.out.printf("------- Poker Scores ------- \n\n");
+    	System.out.printf("\n------- Poker Scores ------- \n\n");
     	System.out.printf("Digite o número de jogadores: ");
     	Scanner sc = new Scanner(System.in);
     	
@@ -66,13 +63,38 @@ public class Program {
             for (int j = 0; j < 2; j++)
             	playerHand.add(deck.removeCard());
             
-            players[i] = new Player(i, new Hand(merge(playerHand, tableCards)));
             players[i] = new Player(i, tableCards, playerHand);
             
             System.out.printf("\nMão do Jogador %d: %s\n", i+1, playerHand.toString());
-            System.out.println("Quanto ele pagou? (digite 0 se ele saiu da mesa) $");
+            System.out.printf("Quanto ele pagou? (digite 0 se ele saiu da mesa) $");
+            players[i].addBet(sc.nextDouble());
             
         }
+        
+        System.out.printf("\nCartas da mesa: ");
+        System.out.println(tableCards.subList(0,3).toString());
+        
+        //---------------------------------Next round of bets---------------------------------
+    	System.out.printf("\n\n------- Poker Scores ------- \n\n");
+        System.out.printf("Segunda rodada de apostas\n");
+        for (int i = 0; i < 2; i++) {
+        	 System.out.printf("\nQuanto o jogador %d pagou? (digite 0 se ele saiu da mesa) $", i+1);
+             players[i].addBet(sc.nextDouble());
+        }
+        
+        System.out.printf("\nCartas da mesa: ");
+        System.out.println(tableCards.subList(0,4).toString());
+        
+        //---------------------------------Next round of bets---------------------------------
+    	System.out.printf("\n\n------- Poker Scores ------- \n\n");
+        System.out.printf("terceira rodada de apostas\n");
+        for (int i = 0; i < 2; i++) {
+        	 System.out.printf("\nQuanto o jogador %d pagou? (digite 0 se ele saiu da mesa) $", i+1);
+             players[i].addBet(sc.nextDouble());
+        }
+        
+        System.out.printf("\nCartas da mesa: ");
+        System.out.println(tableCards.subList(0,5).toString());
         
     }
 
