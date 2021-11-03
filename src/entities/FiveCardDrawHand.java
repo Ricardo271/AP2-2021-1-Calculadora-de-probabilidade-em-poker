c6399350341429d491e7f59245dda92e88dd226a
ph-aquino <pedrohenaquino12@gmail.com>
2021-11-03 11:38:34 -0300

    Adicionadas modalidades de Five Card Draw e 2-7 Low Ball

---
+++ src/entities/FiveCardDrawHand.java
@@ -0,0 +1,32 @@
+package entities;
+
+import java.util.List;
+
+public class FiveCardDrawHand extends Hand{
+	
+	public FiveCardDrawHand(List<Card> hand) {
+		super(hand);
+		strongestHand = hand;
+	}
+	
+	public boolean swap(List<Card> cardsToRemove, List<Card> cardsToAdd) {
+		if (cardsToRemove.size() == cardsToAdd.size()) {
+			for (int i = 0; i < cardsToRemove.size(); i++) {
+				if (!hand.remove(cardsToRemove.get(i))) {
+					System.out.println("ERRO: A carta " + cardsToRemove.get(i) + "não está presente na mão, então não pode ser trocada.");
+					cardsToAdd.remove(i);
+				}
+			}
+			for (int j = 0; j < cardsToAdd.size(); j++) {
+				hand.add(cardsToAdd.get(j));
+			}
+			sortHand(hand);
+			calculatePower();
+			return true;
+		} else {
+			System.out.println("ERRO: A quantidade de cartas adicionadas é diferente da quantidade de cartas a serem removidas.");
+			return false;
+		}
+	}
+	
+}
